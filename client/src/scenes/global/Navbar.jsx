a1a6c1f73d108fdb6ec0668321d56a7d04960267
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-05 18:18:05 +0100

    added the box properties for css styling in navbar

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  MenuOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
 
-const Navbar = () => {
+function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+
   return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255, 255, 255, 0.95)"
+      color="black"
+      position="fixed"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color={shades.secondary[500]}
+        >
+          ECOMMER
+        </Box>
+        </Box>
+      
