324c2f1e3a7d8efd6abeb91c41935fb8ef4e340a
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-05 17:24:30 +0100

    add the routes for itemdetails and checkout

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
 
 
 const ScrollToTop = () => {
@@ -22,6 +24,8 @@ function App() {
       <ScrollToTop />
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="item/:itemId" element={<ItemDetails />} />
+      <Route path="checkout" element={<Checkout />} />
       </Routes>
       </BrowserRouter>
       
