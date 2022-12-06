4416f98c936131365b844d5d9edf138862787244
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-06 16:05:35 +0100

    fixed the error in the shipping.jsx

--- client/src/scenes/checkout/Shipping.jsx
+++ client/src/scenes/checkout/Shipping.jsx
@@ -1,36 +1,68 @@
 
+import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import AddressForm from "./AddressForm";
 
-import React from 'react'
-
-
-<Box
-            position="fixed"
-            right="0"
-            bottom="0"
-            width="max(400px, 30%)"
-            height="100%"
-            backgroundColor="white"
-          ></Box></const> = () => {
+const Shipping = ({
+  values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur,
+  setFieldValue,
+}) => {
   return (
-    <div>
-        <Box
-            position="fixed"
-            right="0"
-            bottom="0"
-            width="max(400px, 30%)"
-            height="100%"
-            backgroundColor="white"
-          ></Box>
-    </const></div>
-  )
-}
+    <Box m="30px auto">
+      {/* BILLING FORM */}
+      <Box>
+        <Typography sx={{ mb: "15px" }} fontSize="18px">
+          Billing Information
+        </Typography>
+        <AddressForm
+          type="billingAddress"
+          values={values.billingAddress}
+          touched={touched}
+          errors={errors}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+      </Box>
 
-export default>
-<Box
-            position="fixed"
-            right="0"
-            bottom="0"
-            width="max(400px, 30%)"
-            height="100%"
-            backgroundColor="white"
-          ></Box></const>
+      <Box mb="20px">
+        <FormControlLabel
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+          label="Same for Shipping Address"
+        />
+      </Box>
+
+      {/* SHIPPING FORM */}
+      {!values.shippingAddress.isSameAddress && (
+        <Box>
+          <Typography sx={{ mb: "15px" }} fontSize="18px">
+            Shipping Information
+          </Typography>
+          <AddressForm
+            type="shippingAddress"
+            values={values.shippingAddress}
+            touched={touched}
+            errors={errors}
+            handleBlur={handleBlur}
+            handleChange={handleChange}
+          />
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Shipping;
