bc96f7f6f94598974da8811c093cb2d6af45c202
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-06 15:53:12 +0100

    import the shipping.jsx file

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -3,43 +3,33 @@ import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
 import { Formik } from "formik";
 import { useState } from "react";
 import * as yup from "yup";
-// import { shades } from "../../theme";
-// import Payment from "./Payment";
+import { shades } from "../../theme";
+import Payment from "./Payment";
 // import Shipping from "./Shipping";
 // import { loadStripe } from "@stripe/stripe-js";
 
 
 const Checkout = () => {
-  return (
-    return (
-      <Box
-        display={isCartOpen ? "block" : "none"}
-        backgroundColor="rgba(0, 0, 0, 0.4)"
-        position="fixed"
-        zIndex={10}
-        width="100%"
-        height="100%"
-        left="0"
-        top="0"
-        overflow="auto"
-      >
-        <Box
-          position="fixed"
-          right="0"
-          bottom="0"
-          width="max(400px, 30%)"
-          height="100%"
-          backgroundColor="white"
-        >
-          <Box padding="30px" overflow="auto" height="100%">
-            {/* HEADER */}
-            <FlexBox mb="15px">
-              <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
-              <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
-                <CloseIcon />
-              </IconButton>
-            </FlexBox>
-  )
+  const [activeStep, setActiveStep] = useState(0);
+  const cart = useSelector((state) => state.cart.cart);
+  const isFirstStep = activeStep === 0;
+  const isSecondStep = activeStep === 1;
+
 }
 
+const handleFormSubmit = async (values, actions) => {
+  setActiveStep(activeStep + 1);
+
+}
+
+async function makePayment(values) { 
+
+}
+
+
+
+
+
+
+  
 export default Checkout
